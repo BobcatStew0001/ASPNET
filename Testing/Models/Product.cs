5141b57234c55beb948774af32ba29073fe63faa
Zach Brown <bigcountry66789@gmail.com>
2026-05-17 15:18:59 -0500

    Completed Exercise 4 of MVC

--- Testing/Models/Product.cs
+++ Testing/Models/Product.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace Testing.Models;
 
 public class Product
@@ -13,4 +15,6 @@ public class Product
     public int CategoryID { get; set; }
     public int OnSale { get; set; }
     public int StockLevel { get; set; }
+    public IEnumerable<Category>Categories { get; set; }
+
 }
